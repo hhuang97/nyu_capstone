8cf7f7e0e49611dd526cde973f9dab2f6a89965c
mindymiao <73625272+mindymiao@users.noreply.github.com>
2020-11-10 15:22:24 -0500

    Add files via upload

--- data_parser.py
+++ data_parser.py
@@ -4,7 +4,7 @@ import pickle
 
 _RAW_DATA_FILE = 'data/CLO_Data.xlsx'
 _PICKLED_DATA_FILE = 'data/all_data.p'
-_STRESS_2016 = 'data/stress_2016.p'
+_STRESS_2016 = 'data/stress_2018.p'
 _STRESS_2020 = 'data/stress_2020.p'
 
 
@@ -13,7 +13,7 @@ def process_data():
     main_df = main_df.set_index('Date')
     with open(_PICKLED_DATA_FILE, 'wb') as f:
         pickle.dump(main_df, f)
-    stress_2016 = main_df[(main_df.index > '2015-12-31') & (main_df.index < '2017-1-1')]
+    stress_2016 = main_df[(main_df.index > '2018-1-1') & (main_df.index < '2018-12-31')]
     stress_2020 = main_df[(main_df.index > '2020-3-1') & (main_df.index < '2020-10-31')]
     with open(_STRESS_2016, 'wb') as f:
         pickle.dump(stress_2016, f)
@@ -21,7 +21,7 @@ def process_data():
         pickle.dump(stress_2020, f)
 
 
-def get_2016_data():
+def get_2018_data():
     with open(_STRESS_2016, 'rb') as f:
         df_2016 = pickle.load(f)
     return df_2016
@@ -41,7 +41,7 @@ def get_all_data():
 
 if __name__ == '__main__':
     process_data()
-    x = get_2016_data()
+    x = get_2018_data()
     y = get_2020_data()
     z = get_all_data()
     print('done')
--- draw_picture.py
+++ draw_picture.py
@@ -5,31 +5,45 @@ Created on Wed Oct 28 22:32:05 2020
 @author: 王睿
 """
 
-x=x.dropna()
+from data_parser import get_all_data, get_2018_data, get_2020_data
 import matplotlib.pyplot as plt
-fig = plt.figure()
+import os
 
-ax1 = fig.add_subplot(111)
-ax1.plot(x.index,x.iloc[:,0],label='S&P',color='r')
-plt.legend()
-ax2 = ax1.twinx()
-ax2.plot(x.index,x.iloc[:,4],label="AAA CLO") 
-#change 4 to [1,3], label should be "CDX","Loan Price" to get the other two pictures
-plt.legend()
-plt.show()
-cor_18=x.corr()
+x = get_2018_data()
+y = get_2020_data()
+z = get_all_data()
 
-y=y.dropna()
-import matplotlib.pyplot as plt
-fig = plt.figure()
+GRAPH_PATH='plots/sp_graph'
 
-ax1 = fig.add_subplot(111)
-ax1.plot(y.index,y.iloc[:,0],label='S&P',color='r')
-plt.legend()
-ax2 = ax1.twinx()
-ax2.plot(y.index,y.iloc[:,4],label="AAA CLO")
-#change 4 to [1,3], label should be "CDX","Loan Price" to get the other two pictures
-plt.legend()
-plt.show()
-cor_20=y.corr()
+#initial correlation analysis
+
+def sp_rel_graph(data,credit_type,year,save=True):
+    x=data.dropna()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(x.index,x['SPX_Index'],label='S&P',color='r')
+    plt.legend()
+    ax2 = ax1.twinx()
+    ax2.plot(x.index,x[credit_type],label=credit_type)
+    #change 4 to [1,3], label should be "CDX","Loan Price" to get the other two pictures
+    plt.legend()
+    if save:
+        plt.savefig(GRAPH_PATH+'/{}.png'.format(year+credit_type))
+
+
+
+if __name__ == '__main__':
+    # 4 is "AAA CLO"
+    #change 4 to [1,3], label should be "CDX","Loan Price" to get the other two pictures
+    credits = x.columns
+    # for i in credits[1:]:
+    #     sp_rel_graph(x,i,'2018')
+    #     sp_rel_graph(y,i,'2020')
+
+    cor_year_2018 = x.corr()
+    cor_year_2018.to_csv('data/2018correlation.csv')
+    cor_year_2020 = y.corr()
+    cor_year_2020.to_csv('data/2020correlation.csv')
+
+
 
